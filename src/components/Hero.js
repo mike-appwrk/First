bacc3f5b2a9bf19f740943fb875c9229a48d1ddb
Mike S <mike.s@appwrk.com>
2022-12-27 12:03:39 +0530

    add hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,6 +3,9 @@ import Button from "./Button";
 function Hero() {
   return (
     <div className="hero">
+      <div className="hero__image">
+        <img src="./images/hero.jpg" alt="Intro" />
+      </div>
       <div className="textbox textbox--hero">
         <h2>Hero</h2>
         <h4>Dream . Build . Grow</h4>
