33107ce539c10dbb12458011ea6b9fdb116ac21c
Mike S <mike.s@appwrk.com>
2022-12-27 12:15:47 +0530

    update hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ function Hero() {
   return (
     <div className="hero">
       <div className="textbox textbox--hero">
-        <h2>Cosmic Waves</h2>
-        <h4>Space Time and Cloud of uncertainity</h4>
+        <h2>Waves</h2>
+        <h4>Space Time and Clouds of uncertainity</h4>
         <Button text="Join Us"></Button>
       </div>
     </div>
