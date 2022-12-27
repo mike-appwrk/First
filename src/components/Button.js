ed03e5360db6683ebcc406af0c43996e00f7ba84
Mike S <mike.s@appwrk.com>
2022-12-27 11:18:31 +0530

    add button to hero

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+function Button ({ text }) {
+  return (
+    <button className="btn btn--primary">
+      {text}
+    </button>
+  ) 
+}
+
+export default Button;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,12 @@
+import Button from "./Button";
+
 function Hero() {
   return (
     <div className="hero">
       <div className="textbox textbox--hero">
         <h2>Hero</h2>
         <h4>Dream . Build . Grow</h4>
+        <Button text="Learn More"></Button>
       </div>
     </div>
   )
